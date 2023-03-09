7c97de5a20fa5a47ac6f1f2016f0c8c3aaef2b1a
Radouane Oubakhane <104201215+radouane-oubakhane@users.noreply.github.com>
2023-03-09 19:02:04 +0100

    add policy iteration actualisation algorith

---
+++ policy_iteration_actualisation.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Les variables globale
+
+#define NUM_STATES 2 // nombre d'états dans le problème
+#define NUM_ACTIONS 2 // nombre d'actions possibles dans chaque état
+#define ALPHA 0.5 // facteur d'actualisation
+
+float V[NUM_STATES]; // Le vecteur gain optimal
+int strategie[NUM_STATES]; // les stratégies
+
+// définir les probabilités de transition et les récompenses pour chaque couple état-action
+float T[NUM_STATES][NUM_ACTIONS][NUM_STATES] = {
+        {{0.3333, 0.6666}, {0.5, 0.5}},
+        {{0.25, 0.75}, {0, 1}}
+};
+float R[NUM_STATES][NUM_ACTIONS] = {{1, 0},
+                                    {3, 2}
+};
+
+void findVAlpha() {
+
+    int i,j,k;
+    float a[10][10]={0.0}, x[10]={0.0};
+    float pivot = 0.0;
+    float factor = 0.0;
+    float sum = 0.0;
+
+
+
+    float I[NUM_STATES][NUM_STATES];
+    for (i = 0; i < NUM_STATES; ++i) {
+        for (j = 0; j < NUM_STATES; ++j) {
+            if (i == j) I[i][j] = 1;
+            else I[i][j] = 0;
+        }
+    }
+
+    float Pf[NUM_STATES][NUM_STATES];
+    for (i = 0; i < NUM_STATES; ++i) {
+        for (j = 0; j < NUM_ACTIONS; ++j) {
+            Pf[i][j] = T[i][strategie[i]][j];
+        }
+    }
+
+    float r[NUM_STATES];
+    for (i = 0; i < NUM_STATES; ++i) {
+        r[i] = R[i][strategie[i]];
+    }
+
+    float temp[NUM_STATES][NUM_STATES];
+    for (i = 0; i < NUM_STATES; ++i) {
+        for (j = 0; j < NUM_ACTIONS; ++j) {
+            temp[i][j] = I[i][j] - (ALPHA * Pf[i][j]);
+        }
+    }
+
+    for (i = 0; i < NUM_STATES; ++i) {
+        for (j = 0; j < NUM_ACTIONS+1; ++j) {
+            if (j == NUM_ACTIONS) a[i+1][j+1] = r[i];
+            else a[i+1][j+1] = temp[i][j];
+        }
+    }
+
+    for(k=1; k <= NUM_STATES - 1; k++) {
+        if(a[k][k]==0.0) {
+            printf("error");
+        }
+        else {
+            pivot = a[k][k];
+            for(j=k; j <= NUM_STATES + 1; j++) a[k][j]= a[k][j]/pivot;
+            for(i=k+1; i <= NUM_STATES; i++) {
+                factor = a[i][k];
+                for(j = k; j <= NUM_STATES + 1; j++) {
+                    a[i][j] = a[i][j] - factor * a[k][j];
+                }
+            }
+        }
+
+        if(a[NUM_STATES][NUM_STATES] == 0) printf("error");
+        else {
+            x[NUM_STATES] = a[NUM_STATES][NUM_STATES + 1] / a[NUM_STATES][NUM_STATES];
+            for(i= NUM_STATES - 1; i >= 1; i--) {
+                sum = 0.0;
+                for(j=i+1; j <= NUM_STATES; j++)
+                    sum = sum + a[i][j] * x[j];
+                x[i]= (a[i][NUM_STATES + 1] - sum ) / a[i][i];
+            }
+        }
+    }
+    for(i=1; i <= NUM_STATES; i++) V[i-1] = x[i];
+}
+
+int findA() {
+    int i,j,a;
+    int ensembleVide;
+    int optimale[NUM_STATES][NUM_ACTIONS];
+
+    float val;
+
+    for (i = 0; i < NUM_STATES; i++) {
+        for (a = 0; a < NUM_ACTIONS; a++) {
+            val = R[i][a];
+            for (j = 0; j < NUM_STATES; j++) {
+                val += ALPHA * T[i][a][j] * V[j];
+            }
+        if (val > V[i]) optimale[i][a] = 0;
+        else optimale[i][a] = 1;
+        }
+    }
+
+    int notOptimale;
+
+    notOptimale = 0;
+
+    for (i = 0; i < NUM_STATES; i++) {
+        ensembleVide = 0;
+        for (j = 0; j < NUM_ACTIONS; j++) {
+            if (optimale[i][j]) ensembleVide = 1;
+            else {
+                ensembleVide = 0;
+                notOptimale = 1;
+                break;
+            }
+        }
+
+        if (!ensembleVide) {
+            for (a = 0; a < NUM_ACTIONS; ++a) {
+                if (strategie[a] != strategie[i]) strategie[i] = strategie[a];
+            }
+        }
+    }
+
+    return notOptimale;
+}
+
+
+int main() {
+    int i, stop = 1;
+
+    // initialiser toutes les stratégies à la première action
+    for (i = 0; i < NUM_STATES; i++) {
+        strategie[i] = 0;
+    }
+
+    // initialiser V
+    findVAlpha();
+
+    while (stop) {
+
+        stop = findA();
+        findVAlpha();
+    }
+
+    printf("La stratégie optimale:\n");
+    for (i = 0; i < NUM_STATES; i++) {
+        printf("Etat : %d -- Action : %d\n", i, strategie[i]);
+    }
+    printf("\nLe vecteur gain optimal: \n");
+    for (i = 0; i < NUM_STATES; i++) {
+        printf("V[%d] = %.2f\n",i ,V[i]);
+    }
+
+    return 0;
+}
